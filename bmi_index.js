f0859dd108aebe5cc37d235ef2485780fe0342e7
Borislav Borislavov <borislavkb@gmail.com>
2021-07-01 18:44:34 +0200

    edit calculation, clear errors

--- bmi_index.js
+++ bmi_index.js
@@ -5,16 +5,24 @@ const weightInput = document.querySelector("#weight");
 
 const bmiMessage = document.querySelector(".result");
 
-const button = document.querySelector("button");
+// const button = document.querySelector("button");
 
 // function calculate(heightInput, weightInput) {
 //   let result = heightInput.value / weightInput.value ** 2;
 //   console.log(result);
 // }
 
-button.addEventListener("click", () => {
-  const result = +heightInput.value / weightInput.value ** 2;
-  console.log(result);
-
+formElement.addEventListener("submit", (event) => {
+  const bmi = (weightInput.value / heightInput.value ** 2) * 10000;
+  event.preventDefault();
+  bmiMessage.textContent = "Your BMI is:" + bmi;
+  if (+bmi < 18.5 || bmi > 30) {
+    bmiMessage.style.color = "red";
+  } else if (18.5 < +bmi <= 25) {
+    bmiMessage.style.color = "green";
+  } else if (25 < +bmi < 30) {
+    bmiMessage.style.color = "orange";
+  }
+  formElement.reset();
   console.log("clicked");
 });
