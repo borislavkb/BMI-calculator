a21c930c95052a8ec30d351a44db14834f1d77a4
Borislav Borislavov <35809916+borislavkb@users.noreply.github.com>
2021-07-05 19:15:26 +0200

    Create bmi_index.js
    Removed console.log and comments in the code.

--- bmi_index.js
+++ bmi_index.js
@@ -5,16 +5,18 @@ const weightInput = document.querySelector("#weight");
 
 const bmiMessage = document.querySelector(".result");
 
+
 formElement.addEventListener("submit", (event) => {
   const bmi = (weightInput.value / heightInput.value ** 2) * 10000;
   event.preventDefault();
-  bmiMessage.textContent = "Your BMI is:" + bmi.toFixed(2);
-  if (bmi < 18.5 || bmi > 30) {
+  bmiMessage.textContent = "Your BMI is:" + bmi;
+  if (+bmi < 18.5 || bmi > 30) {
     bmiMessage.style.color = "red";
-  } else if (18.5 < bmi <= 25) {
+  } else if (18.5 < +bmi <= 25) {
     bmiMessage.style.color = "green";
-  } else if (25 < bmi < 30) {
+  } else if (25 < +bmi < 30) {
     bmiMessage.style.color = "orange";
   }
   formElement.reset();
+  
 });
